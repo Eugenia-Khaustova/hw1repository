2cb5a99240be3978f085c536ebf526bc422f0a6d
Eugenia-Khaustova <yevheniia.khaustova@gmail.com>
2022-02-09 22:27:02 +0200

    added script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let userName = prompt("Enter your name")
+alert("Your name is " + userName)
+
+console.log(userName)
+
+let a = 3;
+let b = 4;
+
+console.log(a,b)
+
+let sum= a+b;
+let substr = a-b;
+let mult = a*b
+let div = a/b
+let exp = a**b
+let resid = b%a
+
+console.log(sum, substr, mult, div, exp, resid)
+
+alert("a+b=" + sum)
+alert("a-b=" + substr)
+alert("a*b=" + mult)
+alert("a/b=" + div)
+alert("a^b=" + exp)
+alert("b%a=" + resid)
